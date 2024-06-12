acda7018c565228e8f1f990734590be0af1d5c2d
Baltasar Junior <meyliyui123@gmail.com>
2024-06-12 02:00:17 -0400

    Normalize line endings

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const buttonNo = document.querySelector('#no')
-const buttonYes = document.querySelector('#yes')
+const buttonNo = document.querySelector('#no');
+const buttonYes = document.querySelector('#yes');
 
-let fontSize = 2
-
+let fontSize = 2;
 let messages = [
   'Estas segura?',
   'Piensalo bien',
   'jejeje xD',
   'Has caso a tu corazon',
   'Aceptalo Sady'
-]
+];
 
 buttonNo.addEventListener('click', () => {
-  fontSize = fontSize + .5
-  buttonYes.style.fontSize = `${fontSize}rem`
+  fontSize = fontSize + 0.5;
+  buttonYes.style.fontSize = `${fontSize}rem`;
 
-  const indexRandom = Math.floor(Math.random() * messages.length)
-
-  buttonNo.textContent = messages[indexRandom]
-})
+  const indexRandom = Math.floor(Math.random() * messages.length);
+  buttonNo.textContent = messages[indexRandom];
+});
 
 buttonYes.addEventListener('click', () => {
-  document.querySelector('#message').style.display = 'flex'
-})
+  document.querySelector('#message').style.display = 'flex';
+});
